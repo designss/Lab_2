debb9015fe2de0b16cd4eb07aa78f9fcd655e42a
designss <tjdwnrtw@gmail.com>
2023-09-05 20:33:19 +0900

    add count.cc

--- Clab2/lab2/count.cc
+++ Clab2/lab2/count.cc
@@ -1,6 +1,12 @@
 #include "count.h"
-
+using namespace std;
 void count_number() {
     // lab2 //
     // do it yourself //
+    for(int i=1; i<11; i++)
+    {
+	    cout << i << endl;
+    }
+    cout << 2018313498 << endl;
+    return;
 }
